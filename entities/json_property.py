5405f19682eeda8daf90e7bff7021afdb9179e65
noc <noc@noc-CF-R3FW1AXR.(none)>
2012-03-30 05:57:36 +0900

    gae 上での json ライブラリの指定の修正
    gae 上に存在しない json ライブラリを使用していたのを修正した。

--- entities/json_property.py
+++ entities/json_property.py
@@ -1,4 +1,4 @@
-import simplejson
+import json
 from google.appengine.ext import db
 
 class JsonProperty(db.TextProperty):
@@ -7,12 +7,12 @@ class JsonProperty(db.TextProperty):
 
     def get_value_for_datastore(self, model_instance):
         result = super(JsonProperty, self).get_value_for_datastore(model_instance)
-        result = simplejson.dumps(result)
+        result = json.dumps(result)
         return db.Text(result)
 
     def make_value_from_datastore(self, value):
         try:
-            value = simplejson.loads(str(value))
+            value = json.loads(str(value))
         except:
             pass
         return super(JsonProperty, self).make_value_from_datastore(value)
